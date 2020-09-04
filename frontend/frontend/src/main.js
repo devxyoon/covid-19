336ef72ff001c9fac69616b58168fab41eb6e06e
devxyoon <o_ov0709@naver.com>
2020-09-04 22:20:59 +0900

    20200904 22:20 Basic Setting of Vue.js

--- frontend/frontend/src/main.js
+++ frontend/frontend/src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Vuex from "vuex";
+import axios from "axios";
+import router from "./router";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.prototype.$http = axios;
+Vue.use(Vuex);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  Vuex,
+  axios,
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
