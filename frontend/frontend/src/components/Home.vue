bb2c7b970dfbdbc6be18aab295d9c4ec8434c7c8
devxyoon <o_ov0709@naver.com>
2020-09-05 11:14:08 +0900

    20200905 11:13 Rendering Korea Map

--- frontend/frontend/src/components/Home.vue
+++ frontend/frontend/src/components/Home.vue
@@ -1,101 +1,69 @@
 <template>
-  <div class="center">
-    <p>
-      For a guide and recipes on how to configure / customize this project,
-      <br />check out the
-      <a
-        href="https://cli.vuejs.org"
-        target="_blank"
-        rel="noopener"
-      >vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-        >babel</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-        >eslint</a>
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a>
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a>
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a>
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a>
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-        >vue-devtools</a>
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a>
-      </li>
-      <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a>
-      </li>
-    </ul>
+  <div>
+    <div class="d3"></div>
   </div>
 </template>
 
 <script>
+import * as d3 from "d3";
+import * as topojson from "topojson";
+
 export default {
   name: "Home",
+  mounted() {
+    this.draw();
+  },
+  methods: {
+    draw() {
+      const koreaMap = require("../assets/skorea-topo-simple.json");
+      const geojson = topojson.feature(
+        koreaMap,
+        koreaMap.objects.skorea_provinces_2018_geo
+      );
+      const width = 1800;
+      const height = 1800;
+      const svg = d3
+        .select(".d3")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+      const map = svg.append("g");
+
+      let projection = d3.geoMercator().scale(1).translate([0, 0]);
+      const path = d3.geoPath().projection(projection);
+      const bounds = path.bounds(geojson);
+      const widthScale = (bounds[1][0] - bounds[0][0]) / width;
+      const heightScale = (bounds[1][1] - bounds[0][1]) / height;
+      const scale = 1 / Math.max(widthScale, heightScale);
+      const xoffset =
+        width / 2 - (scale * (bounds[1][0] + bounds[0][0])) / 2 + 10;
+      const yoffset =
+        height / 2 - (scale * (bounds[1][1] + bounds[0][1])) / 2 + 80;
+      const offset = [xoffset, yoffset];
+      projection.scale(scale).translate(offset);
+
+      map
+        .selectAll("path")
+        .data(geojson.features)
+        .enter()
+        .append("path")
+        .attr("d", path);
+    },
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.center {
+.d3 {
   position: absolute;
   left: 50%;
   top: 50%;
   transform: translateX(-50%) translateY(-50%);
   text-align: center;
 }
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+path {
+  fill: #ed8b70;
+  stroke: #de6a6c;
 }
 </style>
