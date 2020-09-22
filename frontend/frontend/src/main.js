288b5815b8814de0c068465b5877f470eecacf5c
devxyoon <o_ov0709@naver.com>
2020-09-22 11:33:40 +0900

    20200922 11:33 Setting Vuex, {store} on main.js

--- frontend/frontend/src/main.js
+++ frontend/frontend/src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import Vuex from "vuex";
 import axios from "axios";
 import router from "./router";
+import { store } from "./store";
 
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
@@ -12,5 +13,6 @@ new Vue({
   Vuex,
   axios,
   router,
+  store,
   render: (h) => h(App),
 }).$mount("#app");
